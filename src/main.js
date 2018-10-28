154ee730a67c8f3651901747163beb7f3200b5ae
Masahito Okubo 'Mozy <mozy.okubo@gmail.com>
2018-10-29 04:08:17 +0900

    add css lib

--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from "vue"
-import { ApolloClient } from "apollo-client"
-import { HttpLink } from "apollo-link-http"
-import { InMemoryCache } from "apollo-cache-inmemory"
+import Vue from 'vue'
+import { ApolloClient } from 'apollo-client'
+import { HttpLink } from 'apollo-link-http'
+import { InMemoryCache } from 'apollo-cache-inmemory'
 import VueApollo from 'vue-apollo'
 
 import App from './App'
@@ -19,3 +19,16 @@ const apolloClient = new ApolloClient({
 })
 
 Vue.use(VueApollo)
+
+const apolloProvider = new VueApollo({
+  defaultClient: apolloClient
+})
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  apolloProvider,
+  template: '<App/>',
+  components: { App }
+})
